cb31b685f47669f5e8fc0f61b19b5b79a36777e6
Ivan Ponomarev <ivan.ponomarev.pi@gmail.com>
2021-10-27 13:47:29 +0200

    For properties missing implementation of default values for PropertyBatch. And missing optimization of Properties.
    The performance and functionality of Properties is done, and results are outstanding!
    
    6163e493c070382251361366 Finalizing new-features-2 PR/MR

---
+++ src/attributes/PropertyBatch.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spaf\simputils\attributes;
+
+use Attribute;
+use spaf\simputils\generic\BasicAttribute;
+
+/**
+ * @package spaf\simputils\attributes
+ */
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_METHOD)]
+class PropertyBatch extends BasicAttribute {
+
+	const TYPE_SET = 'set';
+	const TYPE_GET = 'get';
+	const TYPE_BOTH = 'both';
+
+	const MODIFIER_PUBLIC = 'public';
+	const MODIFIER_PROTECTED = 'protected';
+	const MODIFIER_PRIVATE = 'private';
+
+	/**
+	 * This is relevant only for ArrayObject similar objects,
+	 * then the values will be stored inside of the object (like ArrayObject enables)
+	 */
+	const STORAGE_SELF = '#SELF';
+
+	public function __construct(
+		public ?string $type = null,
+		public ?string $modifier = null,
+		public ?array $names = null,
+		public ?string $storage = null,
+	) {}
+}

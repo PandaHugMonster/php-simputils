4f6d52867ca55b0987448290f5dcc0534d9c7234
Ivan Ponomarev <pi@spaf.dev>
2022-02-09 12:12:31 +0100

    php-simputils-38 Implement `DataUnit` object
    Blocked by #40

--- src/models/DataUnit.php
+++ src/models/DataUnit.php
@@ -20,6 +20,8 @@ class DataUnit extends SimpleObject {
 
 	const USER_FORMAT_HR = 'hr';
 
+	public static $l10n_translations = null;
+
 	#[DebugHide]
 	protected int $_value;
 	public string $user_format = self::USER_FORMAT_HR;
@@ -55,6 +57,25 @@ class DataUnit extends SimpleObject {
 			return Data::humanReadable($this->_value);
 		}
 		return Data::bytesTo($this->_value, $format).($with_units?Str::upper($format):null);
+	}
+
+	/**
+	 * Translates data-units
+	 *
+	 * An argument must be in initial "english" format. This translator is uni-directional,
+	 * From english abbreviation to another language abbreviation
+	 *
+	 * @param string $name English abbreviation (as constant values)
+	 *
+	 * @return false|mixed
+	 */
+	public static function translator(string $name) {
+		$name = Str::upper($name);
+		$check = new Box(static::$l10n_translations);
+		if ($check->containsKey($name)) {
+			$name = $check[$name];
+		}
+		return $name;
 	}
 
 	public static function redefComponentName(): string {

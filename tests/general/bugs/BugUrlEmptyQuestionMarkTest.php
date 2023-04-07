eaa579c95bb9585fd14f41b9610201f2f7b7d310
Ivan Ponomarev <ivan.ponomarev.pi@gmail.com>
2023-04-07 15:19:50 +0200

    #148 / Url parsing with empty get params is broken
    Fixed

--- tests/general/bugs/BugUrlEmptyQuestionMarkTest.php
+++ tests/general/bugs/BugUrlEmptyQuestionMarkTest.php
@@ -5,13 +5,23 @@ namespace general\bugs;
 use PHPUnit\Framework\TestCase;
 use function spaf\simputils\basic\url;
 
+/**
+ * @covers \spaf\simputils\models\UrlObject
+ * @uses \spaf\simputils\Boolean
+ * @uses \spaf\simputils\PHP
+ * @uses \spaf\simputils\Str
+ * @uses \spaf\simputils\attributes\Property
+ * @uses \spaf\simputils\basic\bx
+ * @uses \spaf\simputils\basic\url
+ * @uses \spaf\simputils\components\normalizers\BooleanNormalizer
+ * @uses \spaf\simputils\components\normalizers\StringNormalizer
+ * @uses \spaf\simputils\models\Box
+ * @uses \spaf\simputils\models\urls\processors\HttpProtocolProcessor
+ * @uses \spaf\simputils\special\CodeBlocksCacheIndex
+ * @uses \spaf\simputils\traits\PropertiesTrait
+ */
 class BugUrlEmptyQuestionMarkTest extends TestCase {
 
-	/**
-	 * @covers \spaf\simputils\models\UrlObject
-	 *
-	 * @return void
-	 */
 	function testOne() {
 		$url = url('localhost', '//test/test?');
 

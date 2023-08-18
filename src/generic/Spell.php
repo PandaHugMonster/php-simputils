b35d32a4099eb6decd863b9bfc4c94f2dced003e
Ivan Ponomarev <ivan.ponomarev.pi@gmail.com>
2023-08-18 18:07:07 +0200

    #161 / Improved meta-magic

---
+++ src/generic/Spell.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spaf\simputils\generic;
+
+use Attribute;
+
+/**
+ * Meta-Magic Spell BaseClass
+ */
+#[Attribute]
+abstract class Spell extends BasicAttribute {
+
+	/**
+	 * Name of the spell class.
+	 *
+	 * Basically a short string name of the Spell
+	 *
+	 * @return mixed
+	 */
+	abstract static function getName(): string;
+
+	/**
+	 * The method that will be invoked for this spell
+	 *
+	 * @param callable $target
+	 * @param mixed    ...$spell
+	 *
+	 * @return mixed
+	 */
+	static function invoke(callable $target, ...$spell): mixed {
+		return $target(...$spell);
+	}
+
+}
